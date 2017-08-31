9476c051d9296e902c9cd488f41903138f6f51ad
wesle <wesle@WESLEY-NOTE>
2017-08-31 02:15:54 -0300

    Final

--- LojadeEletronicos/src/main/java/br/senac/asterix/lojadeeletronicos/dao/DaoProduto.java
+++ LojadeEletronicos/src/main/java/br/senac/asterix/lojadeeletronicos/dao/DaoProduto.java
@@ -6,8 +6,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 public class DaoProduto {
